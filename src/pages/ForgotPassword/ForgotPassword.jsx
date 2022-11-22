3cb42e2a91cb83cf4c93bfde096a4296e2bab3b7
Sai Krishna <saikrishnaa2002@gmail.com>
2022-11-22 11:08:20 +0530

    added forgot password

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -36,24 +36,34 @@ const ForgotPassword = () => {
           <MetaData title="Forgot Password" />
           <div className="forgotPasswordContainer">
             <div className="forgotPasswordBox">
+              <div style={{display:"flex", justifyContent:"center"}}>
+              <img src="/src/assests/forgotPassword.png" height="25px" width="25px" style={{marginTop:"5px", marginRight:"10px"}}></img>
               <h2 className="forgotPasswordHeading">Forgot Password</h2>
-
+              </div>
               <form className="forgotPasswordForm" onSubmit={forgotPasswordSubmit} >
                 <div className="forgotPasswordEmail">
+                  <div>
+                  <label className="ForgotPasswordLabel">EMAIL</label><br></br>
                   <input
                     type="email"
-                    placeholder="Email"
+                    placeholder="Enter your Email Address"
                     required
                     name="Email"
+                    className="ForgotPasswordInput"
                   />
+                  </div>
+                  <div>
+                  <label className="ForgotPasswordLabel">FLAT NUMBER</label><br></br>
                   <input
                     type="text"
                     placeholder="Flat Number"
                     required
                     name="FlatNo"
+                    className="ForgotPasswordInput"
                   />
+                  </div>
                 </div>
-                <button type="submit">Update Password</button>
+                <button type="submit" className="forgotPasswordButton">Update Password</button>
               </form>
             </div>
           </div>
